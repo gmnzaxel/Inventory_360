db89f0c59c5ace17be40026c4e576f5c37a810dd
Axel <screaxgameplays@gmail.com>
2025-04-06 17:57:10 -0300

    agrego de urls

--- control/urls.py
+++ control/urls.py
@@ -5,6 +5,12 @@ from control import views
 
 router = routers.DefaultRouter()
 router.register(r'bussines', views.BussinesView, basename='bussines')
+router.register(r'users', views.UserView, basename='users')
+router.register(r'products', views.ProductView, basename='products')
+router.register(r'categories', views.CategoryView, basename='categories')
+router.register(r'supplier', views.SupplierView, basename='supplier')
+router.register(r'movements', views.MovementView, basename='movements')
+
 
 urlpatterns = [
     path('control/model/', include(router.urls))
