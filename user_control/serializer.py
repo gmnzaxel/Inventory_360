ccc9e55e46fb50f1c08bffa42668e24dc0f8566a
Axel <screaxgameplays@gmail.com>
2025-05-06 19:33:23 -0300

    fix create admin and business

--- user_control/serializer.py
+++ user_control/serializer.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import User
 from control.models import Business
-from control.serializer import BranchSerializer
+from control.serializer import BusinessSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
     password2 = serializers.CharField(write_only=True, required=True)
-    business = BranchSerializer(required=False)
+    business = BusinessSerializer(required=True)
 
     class Meta:
         model = User
@@ -24,36 +24,10 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data.pop('password2')
         password = validated_data.pop('password')
-        business_data = validated_data.pop('business', None)
-
-        # Crear empresa automáticamente si se proporciona (para admin)
-        if business_data:
-            business = Business.objects.create(**business_data)
-        else:
-            business = Business.objects.create(name=f"{validated_data['name']}-Business")
-
-        # Asignar rol 'admin' siempre
-        user = User(
-            role='admin',
-            business=business,
-            **validated_data
-        )
-        user.set_password(password)
-        user.save()
-        return user
-
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        password = validated_data.pop('password')
-        business_data = validated_data.pop('business', None)
-
-        # Crear empresa si el usuario es admin
-        if validated_data.get('role') == 'admin' and business_data:
-            business = Business.objects.create(**business_data)
-            validated_data['business'] = business
-
-        user = User(**validated_data)
+        business_data = validated_data.pop('business')
+        new_business = Business.objects.create(**business_data)
+        validated_data['role'] = 'admin'
+        user = User.objects.create_user(business=new_business, **validated_data)
         user.set_password(password)
         user.save()
         return user
