d54c9cdc939e77870910f2631824be3f1ee5cdda
Axel <screaxgameplays@gmail.com>
2025-05-09 21:17:42 -0300

    fixing paths

--- Inventory360/urls.py
+++ Inventory360/urls.py
@@ -20,8 +20,8 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('control.urls')),
+    path('api/control/', include('control.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/user/', include('user_control.urls')),
+    path('user-control/', include('user_control.urls')),
 ]
--- user_control/urls.py
+++ user_control/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path, include
-from rest_framework import routers
 from .views import RegisterAdminView, LoginView, LogoutView, CreateUserByAdminView
 
 urlpatterns = [
