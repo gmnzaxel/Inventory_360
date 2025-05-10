0446c9cc676827dbcdab595d832ce50c5fc1aa31
Axel <screaxgameplays@gmail.com>
2025-05-09 21:46:41 -0300

    user.required_fields fix

--- user_control/models.py
+++ user_control/models.py
@@ -8,15 +8,11 @@ class User(AbstractUser):
     role = models.CharField(max_length=10, choices=[('admin', 'Admin'), ('user', 'User')])
     business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True, blank=True)
     branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, null=True, blank=True)
-
-    # Nuevos permisos granulares
     can_purchase = models.BooleanField(default=False)
     can_sale = models.BooleanField(default=False)
     can_adjust = models.BooleanField(default=False)
     can_transfer = models.BooleanField(default=False)
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
+    REQUIRED_FIELDS = ['name']
     def __str__(self):
         return f"{self.name} ({self.email})"
